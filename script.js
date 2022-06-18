191346c5a2fe7c1306383dc659e8a3b1d9651045
Badonix <78652512+NickDane2021@users.noreply.github.com>
2022-06-18 16:47:05 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 const hamBurger = document.getElementById("bar");
 const nav = document.getElementById("navbar");
 const closeBtn = document.getElementById("close");
+const invisible = document.querySelector(".invisible");
 
 if (hamBurger) {
   hamBurger.addEventListener("click", function () {
     nav.classList.add("active");
+    invisible.classList.add("big");
   });
 }
 if (closeBtn) {
   closeBtn.addEventListener("click", function () {
     nav.classList.remove("active");
+    invisible.classList.remove("big");
   });
 }
+
+invisible.addEventListener("click", function () {
+  if (nav.classList.contains("active")) {
+    nav.classList.remove("active");
+    invisible.classList.remove("big");
+  }
+});
